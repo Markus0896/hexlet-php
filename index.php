ef91c7ecb59dc8794297b6ff04ad4e8674a60cda
Markus0896 <markmiller0896@gmail.com>
2022-12-22 10:40:33 +0300

    scr/Runner

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-Hexlet one love!
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Файл не включается напрямую
+// Он загрузится автоматически благодаря автозагрузке
+use Hexlet\Php\Runner;
+
+print_r(Runner\run());
+
